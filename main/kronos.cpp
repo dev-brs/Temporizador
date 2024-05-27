0dd703f3231e4dc78250c6be3f2d63d735fbdf0c
brs <bernardo.rodsilva@gmail.com>
2024-05-27 00:14:10 -0300

    algarismo feitos

--- main/kronos.cpp
+++ main/kronos.cpp
@@ -1,31 +1,58 @@
 #include <stdio.h>
-#include "structs.h"
 #include "bugiganga.cpp"
-#include "structs.c"
+#include "matrizes.c"
 
-void printMatriz(struct estruturaDefault estrutura);
+void printMatriz(struct estruturaDefault estrutura, DOS_COLORS corFundo, DOS_COLORS corLetra);
 // system("cls");
 
 int main()
 {
-    printMatriz(nomeStruct);
-    printf("\nhello world!");
-    
+    printMatriz(algarismoZero, BLACK, RED);
+    Sleep(1000);
+    system("cls");
+    printMatriz(algarismoUm, BLACK, RED);
+    Sleep(1000);
+    system("cls");
+    printMatriz(algarismoDois, BLACK, RED);
+    Sleep(1000);
+    system("cls");
+    printMatriz(algarismoTres, BLACK, RED);
+    Sleep(1000);
+    system("cls");
+    printMatriz(algarismoQuatro, BLACK, RED);
+    Sleep(1000);
+    system("cls");
+    printMatriz(algarismoCinco, BLACK, RED);
+    Sleep(1000);
+    system("cls");
+    printMatriz(algarismoSeis, BLACK, RED);
+    Sleep(1000);
+    system("cls");
+    printMatriz(algarismoSete, BLACK, RED);
+    Sleep(1000);
+    system("cls");
+    printMatriz(algarismoOito, BLACK, RED);
+    Sleep(1000);
+    system("cls");
+    printMatriz(algarismoNove, BLACK, RED);
+    Sleep(1000);
+    system("cls");
+    printMatriz(algarismoZero, BLACK, RED);
     return 0;
 }
 
-void printMatriz(struct estruturaDefault estrutura)
+void printMatriz(struct estruturaDefault estrutura, DOS_COLORS corFundo, DOS_COLORS corLetra)
 {
     for (int i = 0; i < estrutura.linhas; i++){
         for (int j = 0; j < estrutura.colunas; j++)
         {
-            if (estrutura.matriz[i * estrutura.colunas + j] == 0)
+            if (estrutura.matriz[i * estrutura.colunas + j])
             {
-                textcolor(BLACK);
+               textcolor(corLetra);
             }
-            if (estrutura.matriz[i * estrutura.colunas + j] == 1)
+            else
             {
-               textcolor(RED);
+                textcolor(corFundo);
             }
             printf("%c", 219);
         }
